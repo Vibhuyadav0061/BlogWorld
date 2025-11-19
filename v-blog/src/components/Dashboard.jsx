9745db800558cf2cbb4ebcbc0f725d790ade885f
VIBHU YADAV <113329071+Vibhuyadav0061@users.noreply.github.com>
2025-11-19 13:18:52 +0530

    Update Dashboard.jsx

--- v-blog/src/components/Dashboard.jsx
+++ v-blog/src/components/Dashboard.jsx
@@ -16,7 +16,7 @@ const Dashboard = ({username}) => {
   useEffect(()=>{
     const  fetchBlogs  = async()=>{
       try{
-        const res = await axios.get(`http://localhost:5000/api/blogs/${username}`);
+        const res = await axios.get(`https://blogworld-od9u.onrender.com/api/blogs/${username}`);
         setBlogs(res.data);
       }
       catch(err){
